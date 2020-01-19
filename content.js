11e389bcfed5a584f1da4bbe9e958cc26787c16b
WofWca <wofwca@gmail.com>
2020-01-19 17:54:59 +0800

    feat: add setting popup

--- content.js
+++ content.js
@@ -1,35 +1,45 @@
-const SILENCE_SPEED = 4;
-const SOUNDED_SPEED = 1;
-const VOLUME_THRESHOLD = 0.01;
-// const SOUNDED_MARGIN_AFTER = 0;
-// const SOUNDED_MARGIN_BEFORE = 40;
-// const SOUNDED_MARGIN_BEFORE_SEC = SOUNDED_MARGIN_BEFORE / 1000;
+chrome.storage.sync.get(
+  // TODO DRY with `popup.js`.
+  {
+    volumeThreshold: 0.012,
+    silenceSpeed: 8,
+    soundedSpeed: 1.75,
+  },
+  function (settings) {
+    const currSettings = { ...settings };
 
-async function controlSpeed(video) {
-  const ctx = new AudioContext();
-  await ctx.audioWorklet.addModule(chrome.runtime.getURL('VolumeGetterProcessor.js'));
-  const volumeGetterNode = new AudioWorkletNode(ctx, 'VolumeGetterProcessor')
-  // TODO audio lags for a moment because of this. Until we connect it to the destination.
-  // Connecting volumeGetter first as the audio will stop playing as soon as we `createMediaElementSource`.
-  volumeGetterNode.port.onmessage = (msg) => {
-    maxChunkVolume = msg.data;
-    // console.log(maxChunkVolume);
-    if (maxChunkVolume < VOLUME_THRESHOLD) {
-      video.playbackRate = SILENCE_SPEED;
-    } else {
-      video.playbackRate = SOUNDED_SPEED;
+    chrome.storage.onChanged.addListener(function (changes) {
+      Object.entries(changes).forEach(([settingName, change]) => {
+        currSettings[settingName] = change.newValue;
+      })
+    });
+
+    async function controlSpeed(video) {
+      const ctx = new AudioContext();
+      await ctx.audioWorklet.addModule(chrome.runtime.getURL('VolumeGetterProcessor.js'));
+      const volumeGetterNode = new AudioWorkletNode(ctx, 'VolumeGetterProcessor')
+      // TODO audio lags for a moment because of this. Until we connect it to the destination.
+      // Connecting volumeGetter first as the audio will stop playing as soon as we `createMediaElementSource`.
+      volumeGetterNode.port.onmessage = (msg) => {
+        maxChunkVolume = msg.data;
+        // console.log(maxChunkVolume);
+        if (maxChunkVolume < currSettings.volumeThreshold) {
+          video.playbackRate = currSettings.silenceSpeed;
+        } else {
+          video.playbackRate = currSettings.soundedSpeed;
+        }
+      }
+      volumeGetterNode.connect(ctx.destination);
+      const src = ctx.createMediaElementSource(video);
+      src.connect(volumeGetterNode);
     }
-  }
-  volumeGetterNode.connect(ctx.destination);
-  const src = ctx.createMediaElementSource(video);
-  src.connect(volumeGetterNode);
-}
 
-const video = document.querySelector('video');
-console.log('Jump Cutter: video:', video);
-if (video !== null) {
-  controlSpeed(video);
-} else {
-  // TODO search again when document updates? Or just after some time?
-  console.log('Jump cutter: no video found. Exiting');
-}
+    const video = document.querySelector('video');
+    // console.log('Jump Cutter: video:', video);
+    if (video === null) {
+      // TODO search again when document updates? Or just after some time?
+      console.log('Jump cutter: no video found. Exiting');
+    }
+    controlSpeed(video);
+  }
+);
---
+++ popup.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const volumeThresholdEl = document.getElementById('volumeThreshold');
+  const silenceSpeedEl = document.getElementById('silenceSpeed');
+  const soundedSpeedEl = document.getElementById('soundedSpeed');
+  const allInputs = [volumeThresholdEl, silenceSpeedEl, soundedSpeedEl];
+
+  chrome.storage.sync.get(
+    {
+      volumeThreshold: 0.012,
+      silenceSpeed: 8,
+      soundedSpeed: 1.75,
+    },
+    function ({ volumeThreshold, silenceSpeed, soundedSpeed }) {
+      volumeThresholdEl.value = volumeThreshold;
+      silenceSpeedEl.value = silenceSpeed;
+      soundedSpeedEl.value = soundedSpeed;
+    }
+  );
+
+  function saveSettings() {
+    chrome.storage.sync.set({
+      volumeThreshold: volumeThresholdEl.value,
+      silenceSpeed: silenceSpeedEl.value,
+      soundedSpeed: soundedSpeedEl.value,
+    });
+  }
+  let saveSettingsDebounceTimeout = -1;
+  function debounceSaveSettings() {
+    clearTimeout(saveSettingsDebounceTimeout);
+    // TODO make sure settings are saved when the popup is closed.
+    saveSettingsDebounceTimeout = setTimeout(saveSettings, 200);
+  }
+  allInputs.forEach(el => {
+    el.addEventListener('input', function () {
+      debounceSaveSettings();
+    });
+  });
+});
