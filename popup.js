b9aeb556814c18b3c428dc3963ec70c68becf634
WofWca <wofwca@gmail.com>
2020-02-16 15:07:01 +0800

    refactor: address popup inputs as list members, not individually

--- popup.js
+++ popup.js
@@ -1,31 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const volumeThresholdEl = document.getElementById('volumeThreshold');
-  const silenceSpeedEl = document.getElementById('silenceSpeed');
-  const soundedSpeedEl = document.getElementById('soundedSpeed');
   const enabledEl = document.getElementById('enabled');
+  const numberInputsNames = [
+    'volumeThreshold',
+    'silenceSpeed',
+    'soundedSpeed',
+  ];
+  const numberInputs = {};
+  numberInputsNames.forEach(n => {
+    numberInputs[n] = document.getElementById(n)
+  });
 
   chrome.storage.sync.get(
     {
+      enabled: true,
       volumeThreshold: 0.01,
       silenceSpeed: 4,
       soundedSpeed: 1.75,
-      enabled: true,
     },
-    function ({ volumeThreshold, silenceSpeed, soundedSpeed, enabled }) {
-      volumeThresholdEl.value = volumeThreshold;
-      silenceSpeedEl.value = silenceSpeed;
-      soundedSpeedEl.value = soundedSpeed;
+    function (settings) {
+      Object.entries(numberInputs).forEach(([name, el]) => {
+        el.value = settings[name];
+      });
       enabledEl.checked = enabled;
     }
   );
 
   function saveSettings() {
-    chrome.storage.sync.set({
-      volumeThreshold: volumeThresholdEl.value,
-      silenceSpeed: silenceSpeedEl.value,
-      soundedSpeed: soundedSpeedEl.value,
+    const newSettings = {
       enabled: enabledEl.checked,
+    };
+    Object.entries(numberInputs).forEach(([name, el]) => {
+      newSettings[name] = parseFloat(el.value);
     });
+    chrome.storage.sync.set(newSettings);
   }
   let saveSettingsDebounceTimeout = -1;
   function debounceSaveSettings() {
@@ -33,8 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // TODO make sure settings are saved when the popup is closed.
     saveSettingsDebounceTimeout = setTimeout(saveSettings, 200);
   }
-  [volumeThresholdEl, silenceSpeedEl, soundedSpeedEl].forEach(el => {
+  enabledEl.addEventListener('input', saveSettings);
+  Object.values(numberInputs).forEach(el => {
     el.addEventListener('input', debounceSaveSettings);
   });
-  enabledEl.addEventListener('input', saveSettings);
 });
