10005120cd676ee93a72e2253eadd889d82a8f1f
WofWca <wofwca@gmail.com>
2020-07-04 19:23:42 +0800

    super good, except silenceStart is emitted a bit earlier than needed

--- content.js
+++ content.js
@@ -29,10 +29,74 @@ function getStretcherDelayChange(snippetOriginalRealtimeDuration, originalSpeed,
   const delayChange = snippetNewDuration - snippetOriginalRealtimeDuration;
   return delayChange;
 }
+// TODO Is it always constant though? What about these short silence snippets, where we don't have to fully reset the margin?
 function getStretcherSoundedDelay(videoTimeMarginBefore, soundedSpeed, silenceSpeed) {
   const realTimeMarginBefore = videoTimeMarginBefore / silenceSpeed;
   const delayChange = getStretcherDelayChange(realTimeMarginBefore, silenceSpeed, soundedSpeed);
   return 0 + delayChange;
+}
+
+// // This function won't help us because on silenceStart we need to know the offset of the moment that has passed.
+// // Wait. But isn't the stretcher's delay on silenceStart always at its max value?
+
+// /**
+//  * Answers the question "When is the sample that is currently on the input going to appear on the output?"
+//  */
+// function getCurrentMomentOutputDelay(lookaheadDelay, lastScheduledStretcherDelayReset) {
+//   return lookaheadDelay + 
+// }
+
+
+
+// /**
+//  * The holy grail of this algorithm.
+//  * Answers the question "When is the sample that has been on the input `ago` seconds ago going to appear on the output?"
+//  * Only works for input values such that the correct answer is after the `lastScheduledStretcherDelayReset`'s start time.
+//  * @param {number} ago - how long ago the target moment has appeared on the input, in seconds.
+//  */
+// function getMomentOutputTime(ago, lookaheadDelay, lastScheduledStretcherDelayReset) {
+//   return lookaheadDelay + 
+// }
+
+/**
+ * The holy grail of this algorithm.
+ * Answers the question "When is the sample that has been on the input at `momentTime` going to appear on the output?"
+ * Contract:
+ * * Only works for input values such that the correct answer is after the `lastScheduledStretcherDelayReset`'s start time.
+ * * Assumes the video is never played backwards (i.e. stretcher delay never so quickly).
+ */
+function getMomentOutputTime(momentTime, lookaheadDelay, lastScheduledStretcherDelayReset) {
+  const stretch = lastScheduledStretcherDelayReset;
+  const stretchEndTotalDelay = getTotalDelay(lookaheadDelay, stretch.endValue);
+  // TODO `const asdadsd = momentTime + stretchEndTotalDelay;` ?
+  // Simpliest case. The target moment is after the `stretch`'s end time
+  if (momentTime + stretchEndTotalDelay >= stretch.endTime) {
+    return momentTime + stretchEndTotalDelay;
+  } else {
+    // `lastScheduledStretcherDelayReset` is going to be in progress when the target moment is on the output.
+    // At which point between its tart and end would the target moment be played?
+    const originalTargetMomentOffsetRelativeToStretchStart =
+      momentTime + getTotalDelay(lookaheadDelay, stretch.startValue) - stretch.startTime;
+    // By how much the snippet is going to be stretched?
+    const playbackSpeedupDuringStretch =
+      ((stretch.endTime - stretch.startTime) + (stretch.startValue - stretch.endValue))
+      / (stretch.endTime - stretch.startTime);
+    // const stretchedSnippetFinalDurationRelativeToOriginalDuration =
+    //   ((stretch.endTime - stretch.startTime) + (stretch.endValue - stretch.startValue))
+    //   / (stretch.endTime - stretch.startTime);
+    // const stretchedSnippetFinalDurationRelativeToOriginalDuration =
+    //   (stretch.endTime - stretch.startTime)
+    //   / ((stretch.endTime - stretch.startTime) + (stretch.startValue - stretch.endValue));
+    // How much time will pass since the stretch start until the target moment is played on the output?
+    const finalTargetMomentOffsetRelativeToStretchStart =
+      originalTargetMomentOffsetRelativeToStretchStart / playbackSpeedupDuringStretch;
+    
+    // Wait what? Stretch start time? Don't we know it already?
+    // const stretchStartTime = momentTime + getTotalDelay(lookaheadDelay, stretch.startValue);
+    // return stretchStartTime + finalTargetMomentOffsetRelativeToStretchStart;
+
+    return stretch.startTime + finalTargetMomentOffsetRelativeToStretchStart;
+  }
 }
 /**
  * Gradually increases/decreases delay of stretcherNode. Not pitch-corrected.
@@ -93,6 +157,8 @@ chrome.storage.sync.get(
     numberSettingsNames.forEach(n => currValues[n] = settings[n]);
 
     async function controlSpeed(video) {
+      let lastScheduledStretcherDelayReset = null;
+
       const ctx = new AudioContext();
       await ctx.audioWorklet.addModule(chrome.runtime.getURL('SilenceDetectorProcessor.js'));
 
@@ -147,6 +213,7 @@ chrome.storage.sync.get(
       }
 
       silenceDetectorNode.port.onmessage = (msg) => {
+        const currentTime = ctx.currentTime;
         const silenceStartOrEnd = msg.data;
         // console.log(silenceStartOrEnd);
         if (silenceStartOrEnd === 'silenceEnd') {
@@ -155,19 +222,105 @@ chrome.storage.sync.get(
           // ALong with speed, the margin changes, because it's real-time, not video-time.
           silenceDetectorNode.parameters.get('durationThreshold').value = getRealtimeMargin(currValues.marginBefore, newSpeed);
 
-          const totalDelay = lookahead.delayTime.value + 0;
-          // Can't just get `silenceDetectorNode.parameters.get('durationThreshold').value`, because the current value
-          // may be different from the value at the moment to which we're scheduling the stretcher delay change.
-          // Same for the `else` block.
-          const oldRealtimeMargin = getRealtimeMargin(currValues.marginBefore, currValues.silenceSpeed);
-          // In practice this is 0 except when stretcher delay reset is in progress.
-          const startIn = totalDelay - oldRealtimeMargin;
-          const originalEndIn = startIn + oldRealtimeMargin;
-          const slowDownBy = currValues.silenceSpeed / currValues.soundedSpeed;
-          scheduleAudioStretch(startIn, originalEndIn, slowDownBy, stretcher, ctx.currentTime);
+          // TODO all this does look like it may cause a snowballing floating point error. Mathematically simplify this?
+          // Or just use if-else?
 
-          // console.log(`Ramping from ${stretcher.delayTime.value} to ${addedDelay} over ${slowDownDuration}s`);
-          // log({ value: addedDelay, endDelta: endTime - ctx.currentTime });
+          // // Can't just get `silenceDetectorNode.parameters.get('durationThreshold').value`, because the current value
+          // // may be different from the value at the moment to which we're scheduling the stretcher delay change.
+          // // Same for the `else` block.
+          // const silenceSpeedRealtimeMarginBefore = getRealtimeMargin(currValues.marginBefore, currValues.silenceSpeed);
+
+          const lastSilenceSpeedLastsForRealtime = currentTime - lastScheduledStretcherDelayReset.newSpeedStartInputTime;
+          const lastSilenceSpeedLastsForVideoTime = lastSilenceSpeedLastsForRealtime * currValues.silenceSpeed;
+
+          const marginBeforePartAtSilenceSpeedVideoTimeDuration = Math.min(
+            lastSilenceSpeedLastsForVideoTime,
+            currValues.marginBefore,
+          );
+          const marginBeforePartAlreadyAtSoundedSpeedVideoTimeDuration =
+            currValues.marginBefore - marginBeforePartAtSilenceSpeedVideoTimeDuration;
+          const marginBeforePartAtSilenceSpeedRealTimeDuration =
+            marginBeforePartAtSilenceSpeedVideoTimeDuration / currValues.silenceSpeed;
+          const marginBeforePartAlreadyAtSoundedSpeedRealTimeDuration =
+            marginBeforePartAlreadyAtSoundedSpeedVideoTimeDuration / currValues.soundedSpeed;
+          // The time at which the moment from which the speed of the video needs to be slow has been on the input.
+          const marginBeforeStartInputTime =
+            currentTime
+            - marginBeforePartAtSilenceSpeedRealTimeDuration
+            - marginBeforePartAlreadyAtSoundedSpeedRealTimeDuration;
+          // Same, but when it's going to be on the output.
+          const marginBeforeStartOutputTime = getMomentOutputTime(
+            marginBeforeStartInputTime,
+            lookahead.delayTime.value,
+            lastScheduledStretcherDelayReset,
+          );
+          const marginBeforeStartOutputTimeTotalDelay = marginBeforeStartOutputTime - marginBeforeStartInputTime;
+          const marginBeforeStartOutputTimeStretcherDelay =
+            marginBeforeStartOutputTimeTotalDelay - lookahead.delayTime.value;
+
+          // As you remember, silence on the input must last for some time before we speed up the video.
+          // We then speed up these sections by calling `scheduleStretcherNodeDelayReset`.
+          // And sometimes we may stumble upon a silence period long enough to make us speed up the video, but short
+          // enough for us to not be done with speeding up that last part, so the margin before and that last part
+          // overlap, and we end up in a situation where we only need to stretch the last part of the margin before
+          // snippet, because the first one is already at required (sounded) speed, due to that delay before we speed up
+          // the video after some silence.
+          // This is also the reason why `getMomentOutputTime` function is so long.
+          // Let's find this breakpoint.
+
+          if (marginBeforeStartOutputTime < lastScheduledStretcherDelayReset.endTime) {
+            // Cancel the complete delay reset, and instead stop decreasing it at `marginBeforeStartOutputTime`.
+            stretcher.delayTime
+              .cancelAndHoldAtTime(marginBeforeStartOutputTime)
+              .linearRampToValueAtTime(marginBeforeStartOutputTimeStretcherDelay, marginBeforeStartOutputTime);
+              // Maybe it's more clear to write this as:
+              // .cancelAndHoldAtTime(lastScheduledStretcherDelayReset.startTime)
+              // .linearRampToValueAtTime(marginBeforeStartOutputTimeStretcherDelay, marginBeforeStartOutputTime)
+            log({
+              type: 'pauseReset',
+              value: marginBeforeStartOutputTimeStretcherDelay,
+              time: marginBeforeStartOutputTime,
+            });
+          }
+
+          const alreadySoundedSpeedPartEndOutputTime =
+            // A.k.a. `marginBeforeStartOutputTime + marginBeforePartAlreadyAtSoundedSpeedRealTimeDuration`
+            lastScheduledStretcherDelayReset.newSpeedStartInputTime
+            + getTotalDelay(lookahead.delayTime.value, marginBeforeStartOutputTimeStretcherDelay);
+          // Need to `setValueAtTime` to the same value again so further `linearRampToValueAtTime` makes increasing the
+          // delay from `alreadySoundedSpeedPartEndOutputTime`.
+          stretcher.delayTime.setValueAtTime(
+            marginBeforeStartOutputTimeStretcherDelay,
+            alreadySoundedSpeedPartEndOutputTime
+          );
+          log({
+            type: 'setValueAtTime',
+            value: marginBeforeStartOutputTimeStretcherDelay,
+            time: alreadySoundedSpeedPartEndOutputTime,
+          });
+          const silenceSpeedPartStretchedDuration = getNewSnippetDuration(
+            marginBeforePartAtSilenceSpeedRealTimeDuration,
+            currValues.silenceSpeed,
+            currValues.soundedSpeed
+          );
+          const stretcherDelayIncrease = getStretcherDelayChange(
+            marginBeforePartAtSilenceSpeedRealTimeDuration,
+            currValues.silenceSpeed,
+            currValues.soundedSpeed
+          );
+          // I think currently it should always be equal to the max delay.
+          const finalStretcherDelay = marginBeforeStartOutputTimeStretcherDelay + stretcherDelayIncrease;
+          stretcher.delayTime.linearRampToValueAtTime(
+            // A.k.a. `marginBeforeStartOutputTimeStretcherDelay + stretcherDelayIncrease`,
+            finalStretcherDelay,
+            // A.k.a. `alreadySoundedSpeedPartEndOutputTime + silenceSpeedPartStretchedDuration`
+            currentTime + getTotalDelay(lookahead.delayTime.value, finalStretcherDelay)
+          );
+          log({
+            type: 'linearRampToValueAtTime',
+            value: marginBeforeStartOutputTimeStretcherDelay + stretcherDelayIncrease,
+            time: alreadySoundedSpeedPartEndOutputTime + silenceSpeedPartStretchedDuration,
+          });
         } else {
           // (Almost) same calculations as obove.
           const newSpeed = currValues.silenceSpeed;
@@ -176,15 +329,37 @@ chrome.storage.sync.get(
           silenceDetectorNode.parameters.get('durationThreshold').value = getRealtimeMargin(currValues.marginBefore, newSpeed);
 
           const oldRealtimeMargin = getRealtimeMargin(currValues.marginBefore, currValues.soundedSpeed);
-          const totalDelay = lookahead.delayTime.value
-            + getStretcherSoundedDelay(currValues.marginBefore, currValues.soundedSpeed, currValues.silenceSpeed);
-          const startIn = totalDelay - oldRealtimeMargin;
+          // When the time comes to increase the video speed, the stretcher's delay is always at its max value.
+          const stretcherDelayStartValue =
+            getStretcherSoundedDelay(currValues.marginBefore, currValues.soundedSpeed, currValues.silenceSpeed);
+          const startIn = getTotalDelay(lookahead.delayTime.value, stretcherDelayStartValue) - oldRealtimeMargin;
 
           const speedUpBy = currValues.silenceSpeed / currValues.soundedSpeed;
-          scheduleStretcherNodeDelayReset(startIn, speedUpBy, stretcher, ctx.currentTime);
+          // scheduleStretcherNodeDelayReset(startIn, speedUpBy, stretcher, ctx.currentTime);
 
-          // log({ value: 0, endDelta: endTime - ctx.currentTime }); // We don't show the delay here actually
-          // console.log(`Ramping from ${stretcher.delayTime.value} to ${0} over ${speeUpDuration}s`);
+          const originalRealtimeSpeed = 1;
+          const delayDecreaseSpeed = speedUpBy - originalRealtimeSpeed;
+          const snippetNewDuration = stretcherDelayStartValue / delayDecreaseSpeed;
+          const startTime = currentTime + startIn;
+          const endTime = startTime + snippetNewDuration;
+          stretcher.delayTime
+            .setValueAtTime(stretcherDelayStartValue, startTime)
+            .linearRampToValueAtTime(0, endTime);
+          lastScheduledStretcherDelayReset = {
+            newSpeedStartInputTime: currentTime,
+            startTime,
+            startValue: stretcherDelayStartValue,
+            endTime,
+            endValue: 0,
+          };
+
+          log({
+            type: 'reset',
+            startValue: stretcherDelayStartValue,
+            startTime: startTime,
+            endTime: endTime,
+            lastScheduledStretcherDelayReset,
+          });
         }
       }
       // setInterval(() => console.log(stretcher.delayTime.value), 10);
