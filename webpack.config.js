80e41c95ecd22acbaa7b6d4016dfb080c5ca4ebb
WofWca <wofwca@gmail.com>
2020-07-13 10:40:03 +0800

    chore: add `serve` script

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  devtool: process.env.NODE_ENV === 'production'
+    // TODO although we generate it, I haven't managed to apply it in Chrome dev tools. Maybe someone will, or it will
+    // get fixed by itself. Why not 'inline-source-map' in prod? Because I believe it would increase parsing time.
+    ? 'source-map'
+    // The default one ('eval') doesn't work because "'unsafe-eval' is not an allowed source of script in the following
+    // Content Security Policy directive:". This occurs when you try to open the popup.
+    : 'inline-source-map',
+
   entry: {
     content: './src/content.js',
     popup: './src/popup.js',
