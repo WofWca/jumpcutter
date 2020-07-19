a0fb21cc486540fc052ae6b66315174e9f75ae5b
WofWca <wofwca@gmail.com>
2020-07-19 16:23:39 +0800

    perf: don't emit source maps in production
    I figured they take a lot of space, especially with these libraries, and almost no one
    is gonna use them, especially considering the fact that they don't work in chrome dev tools
    at all (at least for me).

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const path = require('path');
 
 module.exports = {
   devtool: process.env.NODE_ENV === 'production'
-    // TODO although we generate it, I haven't managed to apply it in Chrome dev tools. Maybe someone will, or it will
-    // get fixed by itself. Why not 'inline-source-map' in prod? Because I believe it would increase parsing time.
-    ? 'source-map'
+    ? undefined
     // The default one ('eval') doesn't work because "'unsafe-eval' is not an allowed source of script in the following
     // Content Security Policy directive:". This occurs when you try to open the popup.
     : 'inline-source-map',
