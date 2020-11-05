637788531f0fe5a626bb8a35a4022a06f2f36b08
WofWca <wofwca@protonmail.com>
2020-11-05 16:58:05 +0800

    perf(hotkeys): defer keydown event handling through setTimeout

--- src/popup/hotkeys.ts
+++ src/popup/hotkeys.ts
@@ -17,7 +17,7 @@ export default async function createKeydownListener(
   // TODO this also assumes that there's an onConnect listener on the other end, but it may not be loaded.
   // TODO make sure it is closed when the listener returned by this function is discarded?
   const nonSettingsActionsPort = chrome.tabs.connect(tabs[0].id!, { name: 'nonSettingsActions' });
-  return (e: KeyboardEvent): void => {
+  const undeferred = (e: KeyboardEvent): void => {
     const settings = getSettings();
     if (eventTargetIsInput(e) && settings.popupDisableHotkeysWhileInputFocused) return;
     // TODO creating a new array on each keydown is not quite good for performance. Or does it get optimized internally?
@@ -29,5 +29,7 @@ export default async function createKeydownListener(
     // by our extension (like "pause" and "increase volume"). Should we add such hotkeys? Or somehow teleport keydown
     // events to the page?
     nonSettingsActionsPort.postMessage(nonSettingsActions);
-  }
+  };
+  // `setTimeout` only for performance.
+  return (e: KeyboardEvent) => setTimeout(undeferred, 0, e);
 }
