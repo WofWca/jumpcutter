957e8560397c57529053ea0d800a7b5730897a59
WofWca <wofwca@protonmail.com>
2020-11-11 14:17:47 +0800

    feat: remove the "experimental features" checkbox
    Stretcher can now be disabled by settings marginBefore to 0

--- src/background/migrations/migrateFrom1_8_0.ts
+++ src/background/migrations/migrateFrom1_8_0.ts
@@ -2,8 +2,8 @@ import { Settings } from "@/settings";
 
 export default async function (): Promise<void> {
   const storage = chrome.storage.local;
-  const { silenceSpeed, soundedSpeed } =
-    await new Promise(r => storage.get(['silenceSpeed', 'soundedSpeed'], r as any));
+  const { silenceSpeed, soundedSpeed, enableExperimentalFeatures } =
+    await new Promise(r => storage.get(['silenceSpeed', 'soundedSpeed', 'enableExperimentalFeatures'], r as any));
   let multiplier = silenceSpeed / soundedSpeed;
   if (!(0 < multiplier && multiplier <= 10)) { // Check if it's reasonable (and if it's a nubmer at all, just in case).
     multiplier = 2;
@@ -12,6 +12,10 @@ export default async function (): Promise<void> {
     silenceSpeedSpecificationMethod: 'relativeToSoundedSpeed',
     silenceSpeedRaw: multiplier,
   }
-  storage.remove('silenceSpeed');
+  if (enableExperimentalFeatures === false) {
+    // Since the new version, this is practically equivalent.
+    newValues.marginBefore = 0;
+  }
+  storage.remove(['silenceSpeed', 'enableExperimentalFeatures']);
   storage.set(newValues);
 }
