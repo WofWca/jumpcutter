b5db89214edfbcae2f64c1643cdef72979618a55
WofWca <wofwca@protonmail.com>
2021-04-25 21:02:51 +0800

    perf: improve Controller settings updating performance

--- src/content/extensionSettings2ControllerSettings.ts
+++ src/content/extensionSettings2ControllerSettings.ts
@@ -4,7 +4,13 @@ import { getAbsoluteSilenceSpeed } from '@/settings';
 
 export default function extensionSettings2ControllerSettings(extensionSettings: ExtensionSettings): ControllerSettings {
   return {
-    ...extensionSettings,
+    // So we don't have to copy all the properties with `...extensionSettings`.
+    soundedSpeed: extensionSettings.soundedSpeed,
+    volumeThreshold: extensionSettings.volumeThreshold,
+    marginBefore: extensionSettings.marginBefore,
+    marginAfter: extensionSettings.marginAfter,
+    enableDesyncCorrection: extensionSettings.enableDesyncCorrection,
+
     silenceSpeed: getAbsoluteSilenceSpeed(extensionSettings),
   };
 }
