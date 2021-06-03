6334dc554a4218c58b5f0fc2eba3f55ee315a4b0
WofWca <wofwca@protonmail.com>
2021-06-03 18:22:29 +0300

    fix: the cloning algorithm not working in Gecko

--- src/content/CloningController/Lookahead.ts
+++ src/content/CloningController/Lookahead.ts
@@ -66,7 +66,13 @@ export default class Lookahead {
     const originalElement = this.originalElement;
 
     // TODO make it depend on `soundedSpeed` and how much silence there is and other stuff.
-    const playbackRate = 5;
+    const playbackRate = BUILD_DEFINITIONS.BROWSER ===  'gecko'
+      // Firefox mutes media elements whose `playbackRate > 4`:
+      // https://hg.mozilla.org/mozilla-central/file/9ab1bb831b50bc4012153f51a75389995abebc1d/dom/html/HTMLMediaElement.cpp#l182
+      // TODO?
+      ? 4
+      // Somewhat arbitrary.
+      : 5;
 
     const clone = this.clone;
 
