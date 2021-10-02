d650f6379dc0dcba1f34fff55f9c1d1499e8b693
WofWca <wofwca@protonmail.com>
2021-10-02 21:34:57 +0800

    fix: popup hanging when you set soundedSpeed to the minimum value (0) in Gecko
    Introduced in e81ad0d
    Closes #30

--- src/background/migrations/runRequiredMigrations.ts
+++ src/background/migrations/runRequiredMigrations.ts
@@ -4,6 +4,7 @@ import migrateFrom1_6_0 from "./migrateFrom1_6_0";
 import migrateFrom1_8_0 from "./migrateFrom1_8_0";
 import migrateFrom1_10_0 from "./migrateFrom1_10_0";
 import migrateFrom1_16_7 from "./migrateFrom1_16_7";
+import migrateFrom1_18_1 from "./migrateFrom1_18_1";
 
 function compareVersions(a: string, b: string) {
   if (process.env.NODE_ENV !== 'production') {
@@ -40,6 +41,7 @@ if (BUILD_DEFINITIONS.BROWSER !== 'gecko') {
 }
 sortedMigrationsFrom.push(
   { ver: '1.16.7', fn: migrateFrom1_16_7, },
+  { ver: '1.18.1', fn: migrateFrom1_18_1, },
 );
 // Post-Firefox extensions store migrations
 // sortedMigrationsFrom.push();
