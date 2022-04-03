WofWca/jumpcutter

commit 02b8ff84458803cdd7827f187fc804d425ca949c

fix(chart): Cloning: volume not being displayed after src change or 'ended' event

src/content/CloningController/CloningController.ts
@@ -2,7 +2,7 @@ import browser from '@/webextensions-api';
 import Lookahead, { TimeRange } from './Lookahead';
 import { assertDev, AudioContextTime, SpeedName } from '@/helpers';
 import type { MediaTime, AnyTime } from '@/helpers';
-import { isPlaybackActive, destroyAudioWorkletNode } from '@/content/helpers';
+import { isPlaybackActive, destroyAudioWorkletNode, requestIdleCallbackPolyfill } from '@/content/helpers';
 import { ControllerKind } from '@/settings';
 import type { Settings as ExtensionSettings } from '@/settings';
 import throttle from 'lodash/throttle';
@@ -251,15 +251,63 @@ export default class Controller {
         captureStream?: () => MediaStream,
         mozCaptureStream?: () => MediaStream,
       }
-      const stream =
-        (element as HTMLMediaElementWithMaybeMissingFields).captureStream?.()
-        || (element as HTMLMediaElementWithMaybeMissingFields).mozCaptureStream?.();
-      if (stream && stream.getAudioTracks().length) {
-        const source = audioContext.createMediaStreamSource(stream);
+      const element_ = element as HTMLMediaElementWithMaybeMissingFields;
+      const captureStream =
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        (element_.captureStream && (() => element_.captureStream!()))
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        || (element_.mozCaptureStream && (() => element_.mozCaptureStream!()));
+
+      if (captureStream) {
+        // The following paragraph is pretty stupid because Web Audio API is still pretty new.
+        // Or because I'm pretty new to it.
+        let source: MediaStreamAudioSourceNode;
+        let reinitScheduled = false;
+        const reinit = () => {
+          source?.disconnect();
+          const newStream = captureStream();
+          assertDev(newStream);
+          // Shouldn't we do something if there are no tracks?
+          if (newStream.getAudioTracks().length) {
+            source = audioContext.createMediaStreamSource(newStream);
+            volumeFilterP.then(filter => source.connect(filter));
+          }
+
+          reinitScheduled = false;
+        }
+        const ensureReinitDeferred = () => {
+          if (!reinitScheduled) {
+            reinitScheduled = true;
+            requestIdleCallbackPolyfill(reinit, { timeout: 2000 });
+          }
+        }
+
+        reinit();
+        // Hopefully this covers all cases where the `MediaStreamAudioSourceNode` stops working.
+        // 'loadstart' is for when the source changes, 'ended' speaks for itself.
+        // https://w3c.github.io/mediacapture-fromelement/#dom-htmlmediaelement-capturestream
+        let unhandledLoadstartOrEndedEvent = false;
+        const onPlaying = () => {
+          if (unhandledLoadstartOrEndedEvent) {
+            ensureReinitDeferred();
+            unhandledLoadstartOrEndedEvent = false;
+          }
+        }
+        element.addEventListener('playing', onPlaying, { passive: true });
+        this._onDestroyCallbacks.push(() => element.removeEventListener('playing', onPlaying));
+        const onLoadstartOrEnded = () => {
+          unhandledLoadstartOrEndedEvent = true;
+        }
+        element.addEventListener('loadstart', onLoadstartOrEnded, { passive: true });
+        element.addEventListener('ended', onLoadstartOrEnded, { passive: true });
+        this._onDestroyCallbacks.push(() => {
+          element.removeEventListener('loadstart', onLoadstartOrEnded);
+          element.removeEventListener('ended', onLoadstartOrEnded);
+        });
+
         const analyser = audioContext.createAnalyser();
         analyser.fftSize = 2 ** 5;
         volumeFilterP.then(volumeFilter => {
-          source.connect(volumeFilter);
           volumeFilter.connect(analyser);
         });
         // Using the minimum possible value for performance, as we're only using the node to get unchanged

commit 5c6080546ecd90bf159f7502c7b302d18afe7edb

fix: `CloningController` crashing when the media is CORS-restricted
Introduced in 02b8ff84
This could happen when `dontAttachToCrossOriginMedia` is `false`

src/content/CloningController/CloningController.ts
@@ -237,12 +237,6 @@ export default class Controller {
       // Also keep in mind that `createMediaElementSource` and `captureStream` are not 100% interchangeable.
       // For example, for `el.volume` doesn't affect the volume for `captureStream()`.
       // TODO fall-back to `createMediaElementSource` if these are not supported?
-      //
-      // TODO if the media is CORS-restricted, this throws an error. You may say that "if it is
-      // CORS-restricted then we won't be able to use the cloning algorithm anyway". Right now you're right,
-      // but in the future we may create the clone inside an <iframe> where it's not CORS-restricted. Wait,
-      // but then it would be leaking information about the contents of the media to the other origin.
-      // Alright, let's discuss it somewhere else.
       type HTMLMediaElementWithMaybeMissingFields = HTMLMediaElement & {
         captureStream?: () => MediaStream,
         mozCaptureStream?: () => MediaStream,
@@ -279,11 +273,22 @@ export default class Controller {
         let reinitScheduled = false;
         const reinit = () => {
           source?.disconnect();
-          const newStream = captureStream();
-          // Shouldn't we do something if there are no tracks?
-          if (newStream.getAudioTracks().length) {
-            source = audioContext.createMediaStreamSource(newStream);
-            volumeFilterP.then(filter => source.connect(filter));
+          let newStream;
+          // `try` because see the `catch` block.
+          try {
+            newStream = captureStream();
+          } catch (e) {
+            if (process.env.NODE_ENV !== 'production') {
+              console.warn('Couldn\'t `captureStream`, but ignoring it because maybe we\'re here because'
+                + ' `dontAttachToCrossOriginMedia` is `false` and the media is CORS-restricted', e);
+            }
+          }
+          if (newStream) {
+            // Shouldn't we do something if there are no tracks?
+            if (newStream.getAudioTracks().length) {
+              source = audioContext.createMediaStreamSource(newStream);
+              volumeFilterP.then(filter => source.connect(filter));
+            }
           }
 
           reinitScheduled = false;