54c01a00391a72347458e11ac4cfc1609c8f1826
WofWca <wofwca@protonmail.com>
2022-04-27 14:20:12 +0300

    refactor: narrow some `Settings` types

--- src/settings/getSettings.ts
+++ src/settings/getSettings.ts
@@ -1,9 +1,9 @@
 import type { Settings } from './';
 import { storage } from './_storage';
 
-export async function getSettings<T extends keyof Settings>(keys: T[]): Promise<Record<T, Settings[T]>>;
-export async function getSettings<T extends keyof Settings>(key: T): Promise<Record<T, Settings[T]>>;
-export async function getSettings<T extends keyof Settings>(defaults: Record<T, Settings[T]>): Promise<Record<T, Settings[T]>>;
+export async function getSettings<T extends keyof Settings>(keys: T[]): Promise<Pick<Settings, T>>;
+export async function getSettings<T extends keyof Settings>(key: T): Promise<Pick<Settings, T>>;
+export async function getSettings<T extends keyof Settings>(defaults: Pick<Settings, T>): Promise<Pick<Settings, T>>;
 export async function getSettings(...args: Parameters<typeof storage.get>): Promise<Settings>;
 export async function getSettings(...args: Parameters<typeof storage.get>): Promise<Settings> {
   return storage.get(...args) as Promise<Settings>;
