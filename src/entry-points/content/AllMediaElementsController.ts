c56237931105239ed344b99474cbd80b8fdee700
WofWca <wofwca@protonmail.com>
2025-09-08 22:33:14 +0400

    refactor: separate time saved badge into function

--- src/entry-points/content/AllMediaElementsController.ts
+++ src/entry-points/content/AllMediaElementsController.ts
@@ -656,93 +656,21 @@ export default class AllMediaElementsController {
 
     // TODO feat: don't require page reload for this settings change
     // to take effect.
+    let sendingTimeSavedMessagesForBadgeP: undefined | Promise<void>
     if (this.settings.badgeWhatSettingToDisplayByDefault === 'timeSaved') {
-      (async () => {
-        let timeSavedPort_:
-          undefined | ReturnType<typeof browserOrChrome.runtime.connect>
-        const getTimeSavedPort = () => {
-          if (timeSavedPort_ == undefined) {
-            // TODO fix: this may connect to two destinations:
-            // "local file player" if it's open
-            // (when `.connect()` gets executed on another website),
-            // and the background script.
-            // In such a case in Chromium `onDisconnect` will not fire
-            // when the background script gets unloaded,
-            // as long as the local file player is open.
-            // This results in the badge not getting updated.
-            // See
-            // - https://bugzilla.mozilla.org/show_bug.cgi?id=1465514
-            // - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/Port#lifecycle
-            // - https://developer.chrome.com/docs/extensions/develop/concepts/messaging#port-lifetime
-            timeSavedPort_ = browserOrChrome.runtime.connect({
-              name: 'timeSavedBadgeText'
-            })
+      let resolveStopPromise: () => void
+      const stopPromise = new Promise<void>(r => resolveStopPromise = r)
+      this._onDetachFromActiveElementCallbacks.push(() => resolveStopPromise())
+      const onStop = (callback: () => void) => stopPromise.then(callback)
 
-            // The port might get disconnected by the remote party,
-            // i.e. the background script getting shut down.
-            // In this case we'll want to open another port.
-            timeSavedPort_.onDisconnect.addListener(() => {
-              timeSavedPort_ = undefined
-            })
-          }
-          return timeSavedPort_
-        }
-        this._onDetachFromActiveElementCallbacks.push(() => {
-          timeSavedPort_?.disconnect()
-          timeSavedPort_ = undefined
-        });
-
-        let lastSentTimeSavedValue: undefined | string = undefined;
-
-        const timeSavedTracker = await timeSavedTrackerPromise
-        const maybeSendTimeSavedInfo = () => {
-          const {
-            wouldHaveLastedIfSpeedWasSounded,
-            timeSavedComparedToSoundedSpeed
-          } = timeSavedTracker.timeSavedData;
-          // Time calculation from `getTimeSavedPlaybackRateEquivalents`.
-          // TODO feat: an option to show speed compared to sounded speed,
-          // and maybe percentage, or absolute value.
-          const timeSaved = (
-            wouldHaveLastedIfSpeedWasSounded /
-            (wouldHaveLastedIfSpeedWasSounded - timeSavedComparedToSoundedSpeed)
-          ).toFixed(2);
-
-          if (lastSentTimeSavedValue !== timeSaved) {
-            getTimeSavedPort().postMessage(timeSaved);
-            lastSentTimeSavedValue = timeSaved;
-          }
-
-          el.removeEventListener('timeupdate', maybeSendTimeSavedInfo)
-          attachListenerAfterIdle()
-        }
-
-        let idleCallbackCancelled = false
-        this._onDetachFromActiveElementCallbacks.push(() => {
-          idleCallbackCancelled = true
-        });
-        const attachListener_ = () => {
-          if (idleCallbackCancelled) {
-            return
-          }
-          el.addEventListener('timeupdate', maybeSendTimeSavedInfo);
-        }
-        const attachListenerAfterIdle = () => {
-          requestIdleCallbackPolyfill(attachListener_)
-        }
-
-        attachListenerAfterIdle()
-        this._onDetachFromActiveElementCallbacks.push(() => {
-          el.removeEventListener('timeupdate', maybeSendTimeSavedInfo)
-        });
-        // Note that we don't remove the listener
-        // when the setting value gets changed.
-      })();
+      sendingTimeSavedMessagesForBadgeP =
+        startSendingTimeSavedMessagesForBadge(el, timeSavedTrackerPromise, onStop);
     }
 
     await controllerP;
     hotkeyListenerP && await hotkeyListenerP;
     await timeSavedTrackerPromise;
+    sendingTimeSavedMessagesForBadgeP && await sendingTimeSavedMessagesForBadgeP
 
     this.ensureAddOnConnectListener();
     // Not doing this at the beginning of the function, beside `this.activeMediaElement = el;` because the popup
@@ -865,4 +793,88 @@ export default class AllMediaElementsController {
       this.debouncedHandleNewElements(this.basicSettings);
     })
   }
+}
+
+async function startSendingTimeSavedMessagesForBadge(
+  el: HTMLMediaElement,
+  timeSavedTrackerPromise: Promise<TimeSavedTracker>,
+  onStop: (callback: () => void) => void,
+) {
+  let timeSavedPort_:
+    undefined | ReturnType<typeof browserOrChrome.runtime.connect>
+  const getTimeSavedPort = () => {
+    if (timeSavedPort_ == undefined) {
+      // TODO fix: this may connect to two destinations:
+      // "local file player" if it's open
+      // (when `.connect()` gets executed on another website),
+      // and the background script.
+      // In such a case in Chromium `onDisconnect` will not fire
+      // when the background script gets unloaded,
+      // as long as the local file player is open.
+      // This results in the badge not getting updated.
+      // See
+      // - https://bugzilla.mozilla.org/show_bug.cgi?id=1465514
+      // - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/Port#lifecycle
+      // - https://developer.chrome.com/docs/extensions/develop/concepts/messaging#port-lifetime
+      timeSavedPort_ = browserOrChrome.runtime.connect({
+        name: 'timeSavedBadgeText'
+      })
+
+      // The port might get disconnected by the remote party,
+      // i.e. the background script getting shut down.
+      // In this case we'll want to open another port.
+      timeSavedPort_.onDisconnect.addListener(() => {
+        timeSavedPort_ = undefined
+      })
+    }
+    return timeSavedPort_
+  }
+  onStop(() => {
+    timeSavedPort_?.disconnect()
+    timeSavedPort_ = undefined
+  });
+
+  let lastSentTimeSavedValue: undefined | string = undefined;
+
+  const timeSavedTracker = await timeSavedTrackerPromise
+  const maybeSendTimeSavedInfo = () => {
+    const {
+      wouldHaveLastedIfSpeedWasSounded,
+      timeSavedComparedToSoundedSpeed
+    } = timeSavedTracker.timeSavedData;
+    // Time calculation from `getTimeSavedPlaybackRateEquivalents`.
+    // TODO feat: an option to show speed compared to sounded speed,
+    // and maybe percentage, or absolute value.
+    const timeSaved = (
+      wouldHaveLastedIfSpeedWasSounded /
+      (wouldHaveLastedIfSpeedWasSounded - timeSavedComparedToSoundedSpeed)
+    ).toFixed(2);
+
+    if (lastSentTimeSavedValue !== timeSaved) {
+      getTimeSavedPort().postMessage(timeSaved);
+      lastSentTimeSavedValue = timeSaved;
+    }
+
+    el.removeEventListener('timeupdate', maybeSendTimeSavedInfo)
+    attachListenerAfterIdle()
+  }
+
+  let idleCallbackCancelled = false
+  onStop(() => (idleCallbackCancelled = true));
+  const attachListener_ = () => {
+    if (idleCallbackCancelled) {
+      return
+    }
+    el.addEventListener('timeupdate', maybeSendTimeSavedInfo);
+  }
+  const attachListenerAfterIdle = () => {
+    requestIdleCallbackPolyfill(attachListener_)
+  }
+
+  attachListenerAfterIdle()
+  onStop(() => {
+    el.removeEventListener('timeupdate', maybeSendTimeSavedInfo)
+  });
+  // Note that we don't remove the listener
+  // when the setting value gets changed.
 }
